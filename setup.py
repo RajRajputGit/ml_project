20758a43a5402f58b74baded6087509098c501d9
RajRajputGit <brajrajput4@gmail.com>
2026-03-24 17:04:25 +0530

    setup.py created

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def read_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name="ml_project",
+    version="0.0.1",
+    author="Braj Rajput",
+    author_email="brajrajputofficial@gmail.com",
+    packages=find_packages(),
+    install_requires=read_requirements("requirements.txt"),
+)
